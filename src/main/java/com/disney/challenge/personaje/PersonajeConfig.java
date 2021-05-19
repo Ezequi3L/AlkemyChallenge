636153164070c5efa5db996b4d323bd4c9a7ed75
Ezequiel Dall'Aglio <ezequieldall@live.com.ar>
2021-05-19 18:14:52 -0300

    test data

--- src/main/java/com/disney/challenge/personaje/PersonajeConfig.java
+++ src/main/java/com/disney/challenge/personaje/PersonajeConfig.java
@@ -12,12 +12,10 @@ public class PersonajeConfig {
     @Bean
     CommandLineRunner commandLineRunner(PersonajeRepository repository){
         return args -> {
-            Personaje ezequiel = new Personaje("ezequiel",22,87F,"autista");
-            Personaje viejo = new Personaje("viejo",22,87F,"autista");
-            Personaje yamato = new Personaje("yamato",22,87F,"autista");
-            Personaje chaca = new Personaje("chaca",22,200F,"autista");
+            Personaje personaje1 = new Personaje("personaje1",18,20F,"");
+            Personaje personaje2 = new Personaje("personaje2",18,20F,"");
 
-            repository.saveAll(List.of(ezequiel,viejo,yamato,chaca));
+            repository.saveAll(List.of(personaje1,personaje2));
 
         };
 
